97aaaa8c0a60cc21bef4b412cce6e6b4b4ce418c
RamonLzSouza <ramonsouza0309@gmail.com>
2019-09-05 20:47:31 -0300

    Criação do Navbar no front end usando bootstrap

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+export default props => (
+    <div>
+        <PageHeader titulo='Sobre' subtitulo='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Nascemos em 2019</p>
+        <h2>Missão e Visão</h2>
+        <p>Atender o cliente com qualidade</p>
+        <h2>Contato</h2>
+        <p>Franca-SP</p>
+    </div>
+)
