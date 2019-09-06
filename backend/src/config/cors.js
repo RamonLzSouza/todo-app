8f92859838bc44c335e27a2fe90a58d49ed744ee
RamonLzSouza <ramonsouza0309@gmail.com>
2019-09-06 07:43:48 -0300

    Criação do evento de adicionar tarefa

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
     //permitir acesso as APIS com métodos GET, POST, PUT e DELETE
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     //configura o cabeçalho da resposta
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content - Type, Accept')
+    res.header('Access-Control-Allow-Headers', '*')
     //executa próxima atividade
     next()
     }
