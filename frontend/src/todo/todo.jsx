8cdf53425ff7fdc72f69428ee6e95da195dba2ac
RamonLzSouza <ramonsouza0309@gmail.com>
2019-09-12 21:44:40 -0300

    Button search

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,15 +15,21 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this);
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
         this.refresh();
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
             .then(resp => this.setState({
-                ...this.state,
-                description: '', list: resp.data
+                ...this.state, description, list:
+                    resp.data
             }))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
     }
 
     handleRemove(todo) {
@@ -59,12 +65,13 @@ export default class Todo extends Component {
                 <TodoForm
                     description={this.state.description}
                     handleAdd={this.handleAdd}
-                    handleChange={this.handleChange} />
+                    handleChange={this.handleChange} 
+                    handleSearch={this.handleSearch}/>
                 <TodoList
                     list={this.state.list}
                     handleRemove={this.handleRemove}
-                    handleMarkAsDone={this.handleMarkAsDone} 
-                    handleMarkAsPending = {this.handleMarkAsPending}/>
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending} />
             </div>
         )
     }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -5,7 +5,7 @@ export default props => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
                         onClick={() =>
@@ -13,7 +13,7 @@ export default props => {
                     <IconButton style='warning' icon='undo' hide={!todo.done}
                         onClick={() =>
                             props.handleMarkAsPending(todo)}></IconButton>
-                    <IconButton style='danger' icon='trash-o'
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
